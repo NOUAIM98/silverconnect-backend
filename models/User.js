e63c5da8b796b83d7bc894375c625b607ab0abea
NOUAIM98 <142490737+NOUAIM98@users.noreply.github.com>
2025-09-14 21:10:28 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -10,13 +10,13 @@ const UserSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
-      unique: true, // ensures no duplicates
+      unique: true, 
       trim: true,
     },
     email: {
       type: String,
       required: true,
-      unique: true, // ensures each email is unique
+      unique: true, 
       lowercase: true,
       trim: true,
     },
